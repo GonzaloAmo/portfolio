7b3cd2cf4bfce8a55cb2c9437ec7a8c6b9ac8291
GonzaloAmo <124767759+GonzaloAmo@users.noreply.github.com>
2024-01-08 19:28:32 +0100

    Commit Generic

---
+++ js/index.js
@@ -0,0 +1,59 @@
+//=============toggele icon navbar============= 
+const menuIcon =document.querySelector("#menu-icon")
+const navBar =document.querySelector(".navbar")
+
+menuIcon.addEventListener("click", ()=>{
+    menuIcon.classList.toggle('bx-x')
+    navBar.classList.toggle('active')
+
+})
+/**=============scroll section active link============= */
+let sections =document.querySelectorAll("section")
+let navlinks =document.querySelectorAll("header nav a")
+window.addEventListener("scroll" ,()=>{
+   
+   
+    sections.forEach(section=>{
+        let top =window.scrollY;
+        let offset=section.offsetTop-150
+        let height =section.offsetHeight
+        let id= section.getAttribute("id")
+        if(top >= offset && top < offset + height){
+            navlinks.forEach(link =>{
+                link.classList.remove('active');
+                document.querySelector("header nav a[href*="  +id+ "]").classList.add('active');
+            })
+        }
+    })
+
+/**============= sticky navbar =============  */
+
+    let header =document.querySelector("header")
+
+    header.classList.toggle('sticky' ,window.scrollY >100)
+
+    menuIcon.classList.remove('bx-x')
+    navBar.classList.remove('active')
+})
+
+
+
+
+
+//=============dawnload cv=============
+const download = document.getElementById("download");
+
+download.addEventListener("click", () => {
+    download.href = "./doc/ENGonzaloAmoCV.pdf";
+    download.target = "_blank";
+    download.download = "ENGonzaloAmoCV.pdf";
+});
+
+
+
+
+//=============send email=============
+
+const form =document.getElementById("contact-form")
+
+
